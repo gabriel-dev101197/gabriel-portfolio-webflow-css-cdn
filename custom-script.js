e6c3205d8a23f7000dcb0f29a7508464636085f1
Gabriel <134805245+gabriel-dev101197@users.noreply.github.com>
2025-12-13 05:09:46 +0000

    change heading text

--- custom-script.js
+++ custom-script.js
@@ -2,6 +2,6 @@
 window.addEventListener("load", function() {
   const heading = document.querySelector("h1");
   if (heading) {
-    heading.textContent = "Custom Code & Custom JS in a Webxlow Free Plan.";
+    heading.textContent = "Custom CSS & Custom JS in a Webxlow Free Plan.";
   }
 });
